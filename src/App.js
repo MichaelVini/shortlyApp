703324c32d8f2ef3b1c853a690cc4adfa5193e02
Michael <michaelvini62@gmail.com>
2020-07-23 22:09:53 -0300

    add footer style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import facebookIcon from './assets/icon-facebook.svg';
 import twitterIcon from './assets/icon-twitter.svg';
 import pinterestIcon from './assets/icon-pinterest.svg';
 import instagramIcon from './assets/icon-instagram.svg';
-import shortenImg from './assets/bg-shorten-desktop.svg';
+import whiteLogoImg from './assets/logo-white.svg';
 import './style.css'
 
 
@@ -43,7 +43,7 @@ function App() {
             <p>Build your brand's recognition and get detailed
             insights on how your links are performing.
           </p>
-            <button>Get Started</button>
+            <button className="btn">Get Started</button>
           </div>
           <div className="sobre-img">
             <img src={workingImg} alt="working" />
@@ -93,22 +93,28 @@ function App() {
           </div>
           <div className="items">
             <ul>
-              <li>
-                <img src={brandImg} alt="Image Brand Recognition" />
+              <li id="brand-li">
+                <div className="img-statistics">
+                  <img src={brandImg} id="brand-img" alt="Image Brand Recognition" />
+                </div>
                 <h1>Brad Recognition</h1>
                 <p>Boost your brand recognition with each click. Generic links don’t
               mean a thing. Branded links help instil confidence in your content.</p>
               </li>
 
-              <li>
-                <img src={detailedImg} alt="Image Detailed Records" />
+              <li id='detailed-li'>
+                <div className="img-statistics" id="detailed-img">
+                  <img src={detailedImg} alt="Image Detailed Records" />
+                </div>
                 <h1>Detailed Records</h1>
                 <p>Gain insights into who is clicking your links. Knowing when and where
               people engage with your content helps inform better decisions.</p>
               </li>
 
-              <li>
-                <img src={customizableImg} alt="Image Fully Customizable" />
+              <li id="customizable-li">
+                <div className="img-statistics" id="customizable-img">
+                  <img src={customizableImg} alt="Image Fully Customizable" />
+                </div>
                 <h1>Fully Customizable</h1>
                 <p>Improve brand awareness and content discoverability through customizable
               links, supercharging audience engagement.</p>
@@ -116,50 +122,54 @@ function App() {
             </ul>
           </div>
         </section>
-
-        <section className="bg-boost">
-          <h1>Boost your link today</h1>
-        </section>
-
-        <section className="footer">
-          <div className="logo">
-            <img src={logoImg} />
-          </div>
-          <div className="list-footer">
-            <ul>
-              <li>
-                <h1>Features</h1>
-                <a href="">Link Shortening</a>
-                <a href="">Braded Links</a>
-                <a href="">Analytics</a>
-              </li>
-
-              <li>
-                <h1>Resources</h1>
-                <a href="">Blog</a>
-                <a href="">Developers</a>
-                <a href="">Support</a>
-              </li>
-
-              <li>
-                <h1>Company</h1>
-                <a href="">About</a>
-                <a href="">Our Team</a>
-                <a href="">Careers</a>
-                <a href="">Contact</a>
-              </li>
-            </ul>
-          </div>
-          <div className="social-media">
-            <ul>
-              <li><img src={facebookIcon} alt="" /></li>
-              <li><img src={twitterIcon} alt="" /></li>
-              <li><img src={pinterestIcon} alt="" /></li>
-              <li><img src={instagramIcon} alt="" /></li>
-            </ul>
-          </div>
-        </section>
       </div>
+      <section className="bg-boost">
+        <h1>Boost your link today</h1>
+        <button className="btn">Get Started</button>
+      </section>
+
+      <section className="footer">
+        <div className="content-footer">
+          <div className="logo">
+            <img src={whiteLogoImg} />
+          </div>
+          <div className="list">
+            <div className="list-footer">
+              <ul>
+                <li>
+                  <h1>Features</h1>
+                  <a href="">Link Shortening</a>
+                  <a href="">Braded Links</a>
+                  <a href="">Analytics</a>
+                </li>
+
+                <li>
+                  <h1>Resources</h1>
+                  <a href="">Blog</a>
+                  <a href="">Developers</a>
+                  <a href="">Support</a>
+                </li>
+
+                <li>
+                  <h1>Company</h1>
+                  <a href="">About</a>
+                  <a href="">Our Team</a>
+                  <a href="">Careers</a>
+                  <a href="">Contact</a>
+                </li>
+              </ul>
+            </div>
+            <div className="social-media">
+              <ul>
+                <li><a href="https://www.facebook.com/" target="_blank"><img src={facebookIcon} alt="" /></a></li>
+                <li><a href="https://twitter.com/" target="_blank"><img src={twitterIcon} alt="" /></a></li>
+                <li><a href="https://br.pinterest.com/" target="_blank"><img src={pinterestIcon} alt="" /></a></li>
+                <li><a href="https://www.instagram.com/?hl=pt-br" target="_blank"><img src={instagramIcon} alt="" /></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
